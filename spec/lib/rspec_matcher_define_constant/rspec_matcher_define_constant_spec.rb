59a8c244fd27d06e5ca121ee2b1dc7aeeba816e9
Pooyan Khosravi <pekhee@gmail.com>
2016-01-06 12:56:35 +0800

    test for example in readme

--- spec/lib/rspec_matcher_define_constant/rspec_matcher_define_constant_spec.rb
+++ spec/lib/rspec_matcher_define_constant/rspec_matcher_define_constant_spec.rb
@@ -1,5 +1,14 @@
 require "spec_helper"
 
-describe "RspecMatcherDefineConstant" do
-  it "should be tested"
+RSpec.describe "a block that defines a constant" do
+  subject do
+    proc do
+      Object.const_set "Stuff", 1
+    end
+  end
+
+  it "defines Stuff" do
+    expect { subject.call }.to define_constant "Stuff"
+    # Stuff is not defined here.
+  end
 end
